2b415e0376571e6f4c2f4c3273bbce9417e84290
Christian Valadez <52517307+chrissvg2323@users.noreply.github.com>
2023-03-12 21:32:21 -0600

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2023, // required
 	month: 5, // required
-	day: 19, // required
-	hours: 9, // Default is 0 [0-23] integer
+	day: 27, // required
+	hours: 4, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
